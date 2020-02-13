ae41822ac4483c5cbc8baf8cc0f68ed30e3f8955
kunal12312 <k1u2n3a4l5@gmail.com>
2020-02-13 23:05:09 +0530

    Test git

--- SpringBootHelloWorld/src/main/java/com/example/SpringBootHelloWorldApplication.java
+++ SpringBootHelloWorld/src/main/java/com/example/SpringBootHelloWorldApplication.java
@@ -3,8 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Change for phase 1branch
-
+//New comment for test
 
 @SpringBootApplication
 public class SpringBootHelloWorldApplication {
